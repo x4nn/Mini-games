d5d48158e09354f31131b0f1801a510dc7aaeed7
xander_detruyer <xander.detruyer@student.howest.be>
2024-04-30 14:09:00 +0200

    added diagonal win mark

--- assets/js/tik-tak-toe-page/game-components/game-loop.js
+++ assets/js/tik-tak-toe-page/game-components/game-loop.js
@@ -7,10 +7,10 @@ const BOARD = [
     [null, null, null]
 ];
 
-function move(e){
+function move(e) {
     const movingPlayer = getMovingPlayer();
 
-    if (clickedOnEmptySquare(e.target)){
+    if (clickedOnEmptySquare(e.target)) {
         renderLetter(e.target, getLetterClass(movingPlayer));
         updateBoard(e.target.dataset.idx);
         updateWinner();
@@ -19,70 +19,69 @@ function move(e){
         renderCurrentPlayer();
     }
 
-    if (WINNER !== null){
+    if (WINNER !== null) {
         document.querySelectorAll('div').forEach($div => {
             $div.removeEventListener('click', move);
         });
         switchMovingPlayer();
-        showWinnigLine();
         setTimeout(gameOver, 500);
     }
 
-    if (boardIsFull()){
+    if (boardIsFull()) {
         switchMovingPlayer(true);
         gameOver();
     }
-    
+
 }//todo fix bug wanneer de laatste zet de winnende zet is
 
-function renderCurrentPlayer(){
+function renderCurrentPlayer() {
     const player = getMovingPlayer();
     const $span = document.querySelector('#curPlayer span');
 
     $span.innerHTML = player;
 }
 
-function clickedOnEmptySquare(square){
+function clickedOnEmptySquare(square) {
     return square.classList.value === '';
 }
 
-function renderLetter(location, letter){
+function renderLetter(location, letter) {
     location.classList.add(letter);
 }
 
-function updateBoard(idx){
+function updateBoard(idx) {
     for (let i = 0; i < BOARD.length; i++) {
         for (let j = 0; j < BOARD[i].length; j++) {
-            if (i * BOARD.length + j === parseInt(idx)){
+            if (i * BOARD.length + j === parseInt(idx)) {
                 addToBoard(i, j);
             }
         }
     }
 }
 
-function addToBoard(i, j){
+function addToBoard(i, j) {
     const movingPlayer = getMovingPlayer();
-    
-    if (movingPlayer === 'p1'){
+
+    if (movingPlayer === 'p1') {
         BOARD[i][j] = 'X';
     } else {
         BOARD[i][j] = 'O';
     }
 }
 
-function updateWinner(){
+function updateWinner() {
     checkHorizontal();
     checkVertical();
     checkDiagonal();
 }
 
-function checkHorizontal(){
+function checkHorizontal() {
     for (let i = 0; i < BOARD.length; i++) {
         let xCounter = 0;
         let oCounter = 0;
 
         for (let j = 0; j < BOARD[i].length; j++) {
-            if (BOARD[i][j] === 'X'){
+            if (BOARD[i][j] === 'X') {
                 xCounter++;
             } else if (BOARD[i][j] === 'O') {
                 oCounter++;
@@ -90,24 +89,24 @@ function checkHorizontal(){
 
             if (xWonHorizontal(xCounter)) {
                 markHorizontalWin(i);
-                WINNER = 'p1';
+                WINNER = 'p1';/*getMovingPlayer()?? */
             } else if (oWonHorizontal(oCounter)) {
                 markHorizontalWin(i);
-                WINNER = 'p2';
+                WINNER = 'p2';/*getMovingPlayer()?? */
             }
         }
     }
 }
 
-function xWonHorizontal(count){
+function xWonHorizontal(count) {
     return count === 3;
 }
 
-function oWonHorizontal(count){
+function oWonHorizontal(count) {
     return count === 3;
 }
 
-function checkVertical(){
+function checkVertical() {
     for (let i = 0; i < BOARD.length; i++) {
         if (checkColumns(i)) {
             WINNER = getMovingPlayer();
@@ -116,48 +115,57 @@ function checkVertical(){
     }
 }
 
-function checkColumns(i){
+function checkColumns(i) {
     return BOARD[0][i] === BOARD[1][i] && BOARD[1][i] === BOARD[2][i] && BOARD[0][i] !== null;
 }
 
-function checkDiagonal(){
-    for (let i = 0; i < BOARD.length; i++) {
-        if (checkDiagonals()) {
-            WINNER = getMovingPlayer();
-        }
+function checkDiagonal() {
+    if (checkDiagonals() === "leftup" || checkDiagonals() === "rightup") {
+        WINNER = getMovingPlayer();
     }
 }
 
-function checkDiagonals(){
-    return ((BOARD[0][0] === BOARD[1][1] && BOARD[1][1] === BOARD[2][2]) ||(BOARD[0][2] === BOARD[1][1] && BOARD[1][1] === BOARD[2][0])) && BOARD[1][1] !== null;
+function checkDiagonals() {
+    if (BOARD[0][0] === BOARD[1][1] && BOARD[1][1] === BOARD[2][2] && BOARD[1][1] !== null){
+        markDiagonalWin(0, 4, 8);
+        return "leftup";
+    }
+    if (BOARD[0][2] === BOARD[1][1] && BOARD[1][1] === BOARD[2][0] && BOARD[1][1] !== null) {
+        markDiagonalWin(2, 4, 6);
+        return "rightup";
+    }
 }
 
-function markVerticalWinningSquares(i){
+function markVerticalWinningSquares(i) {
     const $allSquares = document.querySelectorAll('div');
-    
+
     $allSquares[i].classList.add('win');
-    $allSquares[i+3].classList.add('win');
-    $allSquares[i+6].classList.add('win');
+    $allSquares[i + 3].classList.add('win');
+    $allSquares[i + 6].classList.add('win');
 }
 
-function markHorizontalWin(i){
+function markHorizontalWin(i) {
     const $allSquares = document.querySelectorAll('div');
 
-    $allSquares[i*3].classList.add('win');
-    $allSquares[i*3+1].classList.add('win');
-    $allSquares[i*3+2].classList.add('win');
+    $allSquares[i * 3].classList.add('win');
+    $allSquares[i * 3 + 1].classList.add('win');
+    $allSquares[i * 3 + 2].classList.add('win');
 }
 
-function markDiagonalWin(){
-    
+function markDiagonalWin(i1, i2, i3) {
+    const $allSquares = document.querySelectorAll('div');
+
+    $allSquares[i1].classList.add('win');
+    $allSquares[i2].classList.add('win');
+    $allSquares[i3].classList.add('win');
 }
 
-function boardIsFull(){
+function boardIsFull() {
     let filledSquares = 0;
 
     for (const row of BOARD) {
         for (const col of row) {
-            if (col){
+            if (col) {
                 filledSquares++;
             }
         }
@@ -166,19 +174,11 @@ function boardIsFull(){
     return filledSquares === 9;
 }
 
-function showWinnigLine(){
-    const winningDivs = getWinnigDivs();
-}
-
-function getWinnigDivs(){
-
-}
-
-function gameOver(){
+function gameOver() {
     document.querySelector('#winner').innerHTML = getMovingPlayer();
 
     document.querySelector('main').classList.add('hidden');
     document.querySelector('footer').classList.remove('hidden');
 }
 
-export {move};
+export { move };
