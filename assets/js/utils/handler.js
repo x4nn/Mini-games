2488913cc80a1a489c2e2265db978d33fc0984f3
xander_detruyer <xander.detruyer@student.howest.be>
2024-05-06 21:00:32 +0200

    no html in path

--- assets/js/utils/handler.js
+++ assets/js/utils/handler.js
@@ -2,9 +2,9 @@ function gameHandler(e){
     const game = e.target.id;
     
     if (game === 'tik-tak-toe') {
-        window.location.href = 'tik-tak-toe.html';
+        window.location.href = 'tik-tak-toe';
     } else if (game === 'memory') {
-        window.location.href = 'memory.html';
+        window.location.href = 'memory';
     }
     //add de rest wanneer de vorige klaar is i guess
 }
