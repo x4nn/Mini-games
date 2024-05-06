5497f3c377695e4bc1b4d563cb8a0a627a9eef4a
xander_detruyer <xander.detruyer@student.howest.be>
2024-05-06 10:48:48 +0200

    fixed so you can go to game

--- assets/js/utils/handler.js
+++ assets/js/utils/handler.js
@@ -2,7 +2,9 @@ function gameHandler(e){
     const game = e.target.id;
     
     if (game === 'tik-tak-toe') {
-        window.location.href = 'tik-tak-toe.html'
+        window.location.href = 'tik-tak-toe.html';
+    } else if (game === 'memory') {
+        window.location.href = 'memory.html';
     }
     //add de rest wanneer de vorige klaar is i guess
 }
