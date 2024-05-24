0fd7d0504b82152790e7ac7df570b8b31ca96c73
xander_detruyer <xander.detruyer@student.howest.be>
2024-05-24 10:41:32 +0200

    fixed rainbow bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,13 @@ import { gameHandler} from "./utils/handler.js";
 init();
 
 function init(){
-    //bindEvents();
+    bindEvents();
 }
 
 function bindEvents(){
-    document.querySelectorAll('article').forEach($article => {
-        $article.addEventListener('click', gameHandler);
-    });
+    // document.querySelectorAll('article').forEach($article => {
+    //     $article.addEventListener('click', gameHandler);
+    // });
+
+    document.querySelector('#rainbow').addEventListener('click', (e) => e.preventDefault() );
 }
