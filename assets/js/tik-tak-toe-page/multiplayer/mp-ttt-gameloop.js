294bb8f2c81e6ee8079b25b26be7aa60ef914870
xander_detruyer <xander.detruyer@student.howest.be>
2024-05-26 21:43:56 +0200

    updating board multiplayer works

--- assets/js/tik-tak-toe-page/multiplayer/mp-ttt-gameloop.js
+++ assets/js/tik-tak-toe-page/multiplayer/mp-ttt-gameloop.js
@@ -3,7 +3,7 @@ import * as Firebase from "../../firebase/firebase.js";
 const GAME = 'tiktaktoe';
 const USERNAME = JSON.parse(sessionStorage.getItem('username'));
 
-function startGame(){
+function startGame() {
     console.log('started');
     gameloop();
 }
@@ -14,59 +14,113 @@ function enableMove() {
     });
 }
 
-function unableMove() {
+function disableMove() {
     document.querySelectorAll('.cell').forEach($div => {
         $div.removeEventListener('click', move);
     });
 }
 
 ///data.data.tiktaktoe[code]: da is de game info
-function gameloop(){
+function gameloop() {
     Firebase.getData().then(data => {
+        sessionStorage.setItem('data', JSON.stringify(data));
+
         const gameInfo = data.data.tiktaktoe[sessionStorage.getItem('code')];
+        const board = gameInfo.board;
 
         console.log(gameInfo);
-        
+
         const activePlayer = gameInfo.currentMovePlayer;
 
         if (USERNAME === activePlayer) {
+            refreshBoard(board);
             enableMove();
         } else {
-            unableMove();
             setTimeout(gameloop, 2000);
         }
     });
 }
 
 function move(e) {
-    e.preventDefault();
     console.log('moved');
 
+    disableMove();
+    const game = JSON.parse(sessionStorage.getItem('data')).data[GAME][sessionStorage.getItem('code')];
+    let board = game.board;
+
     if (clickedOnEmptySquare(e.target)) {
-        console.log('empty');
-        // renderLetter(e.target, getLetterClass(movingPlayer));
-        // updateBoard(e.target.dataset.idx);
+        ///------------------------------
+        renderLetter(e.target, getLetter());
+        ///------------------------------
+        const idx = e.target.dataset.idx;
+
+        board = updatedBoard(board, getLetter(), idx);
+        game.board = board;
+        ///------------------------------
         // updateWinner();
 
         // switchMovingPlayer(false);
         // renderCurrentPlayer();
+
     }
 
-    const game =  JSON.parse(sessionStorage.getItem('data')).data[GAME][sessionStorage.getItem('code')];
-    const players = JSON.parse(sessionStorage.getItem('data')).data[GAME][sessionStorage.getItem('code')].players;
+
+    const players = game.players;
     const nextPlayer = getNextPlayer(players);
 
     game.currentMovePlayer = nextPlayer;
 
     Firebase.updateGame(GAME, sessionStorage.getItem('code'), game);
-    // gameloop();
+    gameloop();
+}
+
+function refreshBoard(board){
+    const cells = document.querySelectorAll('.cell');
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === 'X') {
+                //geef die indx op screen bord x class
+                cells[i * board.length + j].classList.add('X');
+            }
+            if (board[i][j] === 'O') {
+                //geef die indx op screen bord o class
+                cells[i * board.length + j].classList.add('O');
+            }
+        }
+    }
+}
+
+function updatedBoard(board, letter, idx) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (i * board.length + j === parseInt(idx)) {
+                board[i][j] = letter;
+            }
+        }
+    }
+    return board;
+}
+
+function getLetter() {
+    const players = JSON.parse(sessionStorage.getItem('data')).data[GAME][sessionStorage.getItem('code')].players;
+
+    for (const player of players) {
+        if (player.pname === USERNAME) {
+            return player.pion;
+        }
+    }
+    return null;
+}
+
+function renderLetter(location, letter) {
+    location.classList.add(letter);
 }
 
 function clickedOnEmptySquare(square) {
     return square.classList.contains('cell') && !square.classList.contains('X') && !square.classList.contains('O');
 }
 
-function getNextPlayer(players){
+function getNextPlayer(players) {
     for (const player of players) {
         if (player.pname !== USERNAME) {
             return player.pname;
@@ -75,4 +129,4 @@ function getNextPlayer(players){
     return 'null';
 }
 
-export {startGame};
+export { startGame };
