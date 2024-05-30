ad72bc44a27b49bb9a9cbf7e5d42d0667b190ece
xander_detruyer <xander.detruyer@student.howest.be>
2024-05-30 16:42:02 +0200

    fixed not being able to play TTT

--- assets/js/main-page/handler.js
+++ assets/js/main-page/handler.js
@@ -1,7 +1,6 @@
 import { loadFromStorage, saveToStorage, toggleHidden } from "../util.js";
 
 function handleHover(e) {
-    console.log(e);
     if (e.type === 'mouseenter') {
         e.target.querySelector('p').classList.toggle('hidden');
     } else {
@@ -12,9 +11,8 @@ function handleHover(e) {
 function gameHandler(e) {
     e.preventDefault();
 
-
     const game = e.target.id;
-    if (e.target.parentElement.parentElement.id === 'online') {
+    if (e.target.parentElement.parentElement.parentElement.id === 'online') {
         if (isReadyToPlay()) {
             const username = document.querySelector('#username').value;
             saveToStorage('username', username);
